f2c85adb98a485aca3210e010233b31d9899709a
Émile Ménard-Croteau <emile.menard@outlook.com>
2019-02-11 14:00:25 -0500

    commit

--- js/base.js
+++ js/base.js
@@ -18,10 +18,58 @@ function createCol(data) {
 }
 
 $("#InsertShowButton" ).click(function(event) {
-      $("#Tableau-2").show();
+      $("#Tableau-Controls").show();
       $(this).hide();
-    });
+});
+
+$("#ControlAcceptButton" ).click(function(event) {
+      var name = $('#InsertName').val();
+      var telephone = $('#InsertTelephone').val();
+      var email = $('#InsertEmail').val();
+      
+      if(validationProvider.isValid())
+            insertRow(name, email, telephone);
+     
+});
+
+function validate_email(){
+      let TBX_Email = document.getElementById("InsertEmail");
+      let emailRegex = /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/;
+
+      if (!emailRegex.test(TBX_Email.value))
+          return "Adresse de courriel invalide";
+
+      return "";
+  }
+  function validate_name(){
+      let TBX_FirstName = document.getElementById("InsertName");
+
+      if (TBX_FirstName.value === "")
+          return "Prénom manquant";
+      
+      return "";
+  }
+
+  function validate_telephone(){
+      let TBX_Phone = document.getElementById("InsertTelephone");
+
+      if (TBX_Phone.value === "")
+          return "Téléphone manquant";
+      
+      return "";
+  }
+
+
+
+
 
 insertRow('Samuel', '00000000', 'sam@outlook.com');
 insertRow('Jean', '00000000', 'jean.pierre@outlook.com');
 insertRow('Audrey', '00000000', 'audrey.vigneux@outlook.com');
+
+
+let validationProvider = new ValidationProvider("Form-Insert");
+
+validationProvider.addControl("InsertName", validate_name);
+validationProvider.addControl("InsertEmail", validate_email);
+validationProvider.addControl("InsertTelephone", validate_telephone);
