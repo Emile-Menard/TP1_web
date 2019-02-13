e4e2b3d98ac72a03c914eaa4aaad403846aa581f
vincent <falardeau.vince@gmail.com>
2019-02-13 11:57:15 -0500

    Code js nettoyé et fixation du fonctionnement

--- js/base.js
+++ js/base.js
@@ -13,80 +13,64 @@ function insertRow(nom, telephone, courriel) {
             }
 
             var deleteButton = document.createElement("button");
-            
-            var EditButton = document.createElement("button");
-            
-            EditButton.classList.add('invisible');
             deleteButton.classList.add('invisible');
 
-
+            var editButton = document.createElement("button");
+            editButton.classList.add('invisible');
+            
             var col = document.createElement("div");
             col.classList.add("table-cell");
-            col.appendChild(EditButton);
+            col.appendChild(editButton);
             col.appendChild(deleteButton);
-            EditButton.innerHTML = '<i class="fa fa-pencil"></i>';
- 
+
+            editButton.innerHTML = '<i class="fa fa-pencil"></i>';
             deleteButton.style.marginLeft= '5px';
             deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-
-            
-            $(row).on('mouseover',  function(e) {
-              $(this).find('button').show();
-            });
-
-
-
-
-
 
             let overlay_ = document.getElementById("Overlay");
             let back = document.getElementById("Wrapper");
             let popup_ = document.getElementById("Popup");
 
-            $(deleteButton).on('click',  function(e) {
-              currentRow = $(this).parent().parent();
-              console.log($(this))
-              overlay_.classList.add('show');
-              popup_.classList.remove('invisible');
-              back.classList.add('disabled');
-             
+            let delAcceptBtn = document.getElementById("DeleteAcceptButton");
+            let delDenyBtn = document.getElementById("DeleteDenyButton");
+
+            $(row).on('mouseover',  function(e) {
+                $(this).find('button').show();
+            });
+            
+            $(row).on('mouseout',  function(e) {
+                $(this).find('button').hide();
             });
 
-            let delAccept = document.getElementById("DeleteAcceptButton");
+            $(deleteButton).on('click',  function(e) {
+                currentRow = $(this).parent().parent();
+                console.log($(this))
+                overlay_.classList.add('show');
+                popup_.classList.remove('invisible');
+                back.classList.add('disabled');
+            });
 
-             $(delAccept).on('click',  function(e) {
+            $(editButton).on('click',  function(e) {
+                let row = $(this).parent().parent();
+                updateData(row);
+            });
+
+
+            $(delAcceptBtn).on('click',  function(e) {
                 currentRow.remove();
                 overlay_.classList.remove('show');
                 popup_.classList.add('invisible');
-                back.classList.remove('disabled');             
-             });
+                back.classList.remove('disabled'); 
+                clearControls();            
+            });
 
-             let delDeny = document.getElementById("DeleteDenyButton");
-
-             $(delDeny).on('click',  function(e) {
+            $(delDenyBtn).on('click',  function(e) {
                 
                 overlay_.classList.remove('show');
                 popup_.classList.add('invisible');
                 back.classList.remove('disabled');             
-              });
-
-
-
-
-
-
-            
-            $(row).on('mouseout',  function(e) {
-             $(this).find('button').hide();
             });
 
-            $(EditButton).on('click',  function(e) {
-             
-             let row = $(this).parent().parent();
-              
-             updateData(row);
-            
-            });
             row.appendChild(col);
 
             table.append(row);
@@ -94,127 +78,122 @@ function insertRow(nom, telephone, courriel) {
 }
 
 function updateData(row) {
- 
-  let nom = row.children()[0].innerHTML;
-  let telephone = row.children()[1].innerHTML;
-  let courriel = row.children()[2].innerHTML;
+    let nom = row.children()[0].innerHTML;
+    let telephone = row.children()[1].innerHTML;
+    let courriel = row.children()[2].innerHTML;
   
-  let controlTab =  $("#Tableau-Controls");
-  if(!controlTab.is(":visible")){
-    controlTab.show();
-    $("#InsertShowButton" ).hide();
-  
-  }
+    let controlTab =  $("#Tableau-Controls");
+    if(!controlTab.is(":visible")){
+        controlTab.show();
+        $("#InsertShowButton" ).hide();
+    }
 
-  validationProvider.reset();
+    validationProvider.reset();
 
-  $('#InsertName').val(nom);
-  $('#InsertTelephone').val(telephone);
-  $('#InsertEmail').val(courriel);
+    $('#InsertName').val(nom);
+    $('#InsertTelephone').val(telephone);
+    $('#InsertEmail').val(courriel);
 
-  updating = {isUpdating: true, row: row};
-
+    updating = {isUpdating: true, row: row};
 }
 
 function createCol(data) {
-      var col = document.createElement("div");
-      col.innerHTML = data;
-      col.classList.add("table-cell");
-      return col;
+    var col = document.createElement("div");
+    col.innerHTML = data;
+    col.classList.add("table-cell");
+    return col;
 }
 
 $("#InsertShowButton").click(function(event) {
-      $("#Tableau-Controls").show();
-      $(this).hide();
-      validationProvider.reset();
+    $("#Tableau-Controls").show();
+    $(this).hide();
+    validationProvider.reset();
 });
 
-$("#ControlDenyButton").click(function(event) {
-      clearSoft();
+$("#ControlRefreshButton").click(function(event) {
+    clearInputs();
 });
+
 $("#ControlCancelButton").click(function(event) {
-     clearControls();
+    clearControls();
 });
 
 
 function clearControls() {
-      $('#InsertName').val('');
-      $('#InsertTelephone').val('');
-      $('#InsertEmail').val('');
-      updating = {isUpdating: false, row: null};
+    $('#InsertName').val('');
+    $('#InsertTelephone').val('');
+    $('#InsertEmail').val('');
+    updating = {isUpdating: false, row: null};
 
-      $("#Tableau-Controls").hide();
-      $("#InsertShowButton" ).show();
+    $("#Tableau-Controls").hide();
+    $("#InsertShowButton" ).show();
      
 }
 
-function clearSoft() {
-      $('#InsertName').val('');
-      $('#InsertTelephone').val('');
-      $('#InsertEmail').val('');
-      updating = {isUpdating: false, row: null};
-
-     
+function clearInputs() {
+    $('#InsertName').val('');
+    $('#InsertTelephone').val('');
+    $('#InsertEmail').val('');
+    updating = {isUpdating: false, row: null}; 
 }
 
 
 $("#Form-Insert").submit(function( event ) {
-      var name = $('#InsertName').val();
-      var telephone = $('#InsertTelephone').val();
-      var email = $('#InsertEmail').val();
+    var name = $('#InsertName').val();
+    var telephone = $('#InsertTelephone').val();
+    var email = $('#InsertEmail').val();
       
-      if(validationProvider.isValid()) {
+    if(validationProvider.isValid()) {
         if(updating.isUpdating){
          
-         $(updating.row).children()[0].innerHTML = name;
-         $(updating.row).children()[1].innerHTML = telephone;
-         $(updating.row).children()[2].innerHTML = email;
-         clearControls();
-        }else{
-          insertRow(name, telephone, email); 
-          clearControls();
+        $(updating.row).children()[0].innerHTML = name;
+        $(updating.row).children()[1].innerHTML = telephone;
+        $(updating.row).children()[2].innerHTML = email;
+        clearControls();
+        }
+        else{
+            insertRow(name, telephone, email); 
+            clearControls();
         }
         
-      }
-            
+    }         
 });
 
 function validate_email(){
-      let TBX_Email = document.getElementById("InsertEmail");
-      let emailRegex = /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/;
+    let TBX_Email = document.getElementById("InsertEmail");
+    let emailRegex = /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/;
 
-      if (!emailRegex.test(TBX_Email.value))
-          return "Adresse de courriel invalide";
+    if (!emailRegex.test(TBX_Email.value))
+        return "Adresse de courriel invalide";
 
-      return "";
-  }
-  function validate_name(){
-      let TBX_FirstName = document.getElementById("InsertName");
+    return "";
+}
 
-      if (TBX_FirstName.value === "")
-          return "Prénom manquant";
+function validate_name(){
+    let TBX_FirstName = document.getElementById("InsertName");
+
+    if (TBX_FirstName.value === "")
+        return "Prénom manquant";
       
-      return "";
-  }
+    return "";
+}
 
-  function validate_telephone(){
-      let TBX_Phone = document.getElementById("InsertTelephone");
+function validate_telephone(){
+    let TBX_Phone = document.getElementById("InsertTelephone");
 
-      if (TBX_Phone.value === "" )
-          return "Téléphone manquant";
-      if (!(TBX_Phone.value.length === 10 && $.isNumeric(TBX_Phone.value) && TBX_Phone.value > 0)) {
+    if (TBX_Phone.value === "" )
+        return "Téléphone manquant";
+    if (!(TBX_Phone.value.length === 10 && $.isNumeric(TBX_Phone.value) && TBX_Phone.value > 0)) {
         return "Téléphone de format invalide";
-      }
+    }
       
-      return "";
-  }
-
+    return "";
+}
 
 
 insertRow('Samuel', '1234567890', 'sam@outlook.com');
 insertRow('Jean', '1234567890', 'jean.pierre@outlook.com');
 insertRow('Audrey', '1234567890', 'audrey.vigneux@outlook.com');
-
 
 let validationProvider = new ValidationProvider("Form-Insert");
 
